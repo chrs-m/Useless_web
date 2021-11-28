56890c843a1b34c2be64ee06a89320288adc1c39
chrs-m <mister.ch@gmail.com>
2021-11-28 20:34:37 +0100

    add Snarky functionality

--- QUIZ/JS/snarky.js
+++ QUIZ/JS/snarky.js
@@ -1,4 +1,5 @@
 const snarky = document.querySelector(".snarky");
+const triangle = document.querySelector(".triangle");
 const commentBox = document.querySelector(".comment");
 
 // SNARKY COMMENTS
@@ -13,12 +14,63 @@ const snarkyComments = [
   "Hello darkness my old friend..",
 ];
 
+// PRESS FOR RANDOM SNARKY COMMENT
 snarky.addEventListener("click", () => {
   commentBox.classList.remove("hide");
   commentBox.innerText =
     snarkyComments[Math.floor(Math.random() * snarkyComments.length)];
 });
 
+//BACKGROUNDCOLORS
+const backgroundColors = [
+  "white",
+  "red",
+  "blue",
+  "green",
+  "hotpink",
+  "limegreen",
+];
+
+// PRESS TO HIDE SNARKY COMMENT
 commentBox.addEventListener("click", () => {
   commentBox.classList.add("hide");
+});
+
+// LISTEN FOR EVENT (FINDING KEYCODES)
+document.addEventListener("keydown", (keyPress) => {
+  //   console.log(event);
+});
+
+// SNARKY FUNCTIONS
+// ARROW RIGHT FUNCTION
+document.addEventListener("keydown", (e) => {
+  if (event.keyCode === 39) {
+    // console.log("moving right");
+    triangle.style.borderBottomColor =
+      backgroundColors[Math.floor(Math.random() * backgroundColors.length)];
+  }
+});
+// ARROW LEFT FUNCTION
+document.addEventListener("keydown", (e) => {
+  if (event.keyCode === 37) {
+    // console.log("moving left");
+    triangle.style.borderBottomColor =
+      backgroundColors[Math.floor(Math.random() * backgroundColors.length)];
+  }
+});
+// ARROW DOWN FUNCTION
+document.addEventListener("keydown", (e) => {
+  if (event.keyCode === 40) {
+    // console.log("moving down");
+    snarky.style.backgroundColor =
+      backgroundColors[Math.floor(Math.random() * backgroundColors.length)];
+  }
+});
+// ARROW UP FUNCTION
+document.addEventListener("keydown", (e) => {
+  if (event.keyCode === 38) {
+    // console.log("moving up");
+    snarky.style.backgroundColor =
+      backgroundColors[Math.floor(Math.random() * backgroundColors.length)];
+  }
 });
