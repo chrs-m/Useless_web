f5cb9b2803d3518749b21e919a257bfbb28998ef
chrs-m <mister.ch@gmail.com>
2021-12-06 14:31:48 +0100

    Add new dad joke API, new media queries

--- JS/dadjoke.js
+++ JS/dadjoke.js
@@ -1,21 +1,41 @@
-// REQUEST CONST
-const myRequest = new Request("./api/jokes.json");
-
 // PRESS FOR JOKE FUNCTION
 const dadEmoji = document.querySelector(".dad-emoji");
 const jokeContainer = document.querySelector(".dad-joke");
 
-dadEmoji.addEventListener("click", () => {
-  jokeContainer.classList.remove("hide");
-  fetch(myRequest)
-    .then((response) => response.json())
-    .then((jokes) => {
-      jokeContainer.classList.remove("hide");
-      jokeContainer.innerText = jokes[Math.floor(Math.random() * jokes.length)];
-    })
-    .catch(console.error);
-});
+// RUN THIS CODE FOR LOCAL API
 
+// // REQUEST CONST
+// const myRequest = new Request("api/jokes.json");
+// dadEmoji.addEventListener("click", () => {
+//   jokeContainer.classList.remove("hide");
+//   fetch(myRequest)
+//     .then((response) => response.json())
+//     .then((jokes) => {
+//       jokeContainer.classList.remove("hide");
+//       jokeContainer.innerText = jokes[Math.floor(Math.random() * jokes.length)];
+//     })
+//     .catch(console.error);
+// });
+
+// ----------------------------------------------------------------
+
+// RUN THIS CODE TO USE DAD JOKE API
+
+dadEmoji.addEventListener("click", generateJokes);
+
+async function generateJokes() {
+  const jokes = await fetch("https://icanhazdadjoke.com/", {
+    headers: {
+      Accept: "application/json",
+    },
+  });
+  const joke = await jokes.json();
+
+  jokeContainer.classList.remove("hide");
+  jokeContainer.innerText = joke.joke;
+}
+
+// TO HIDE THE JOKE CONTAINER
 jokeContainer.addEventListener("click", () => {
   jokeContainer.classList.add("hide");
 });
